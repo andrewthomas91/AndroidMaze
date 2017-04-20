cfab7353c75c4610cda9d2977e3648373257d485
Andrew Thomas <andrew.thomas@workday.com>
2017-04-20 16:27:56 -0700

    inport cleanup

--- app/src/main/java/com/andrewthomas/hackathonmaze/CanvasView.java
+++ app/src/main/java/com/andrewthomas/hackathonmaze/CanvasView.java
@@ -6,14 +6,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
-import java.util.zip.DeflaterInputStream;
 
 public class CanvasView extends View {
 
