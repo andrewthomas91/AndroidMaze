51739fa1bed3619f55e8c7a88d64a735922daac6
Andrew Thomas <andrew.thomas@workday.com>
2017-04-20 14:32:38 -0700

    New Files

--- app/src/main/java/com/andrewthomas/hackathonmaze/CanvasView.java
+++ app/src/main/java/com/andrewthomas/hackathonmaze/CanvasView.java
@@ -245,18 +245,6 @@ public class CanvasView extends View {
         });
     }
 
-    private Thread createNewSleepThread() {
-        return new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Thread.sleep(250);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
     private Directions directionOfClickLocation(float x, float y) {
         double angleInDegrees = 0;
         if ((x > centerX)) {
